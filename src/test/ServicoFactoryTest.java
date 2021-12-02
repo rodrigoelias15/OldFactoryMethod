d6da3c2c5662421a2bbd95338858bca5762906e3
Rodrigo <rodrigo.elias@engenharia.ufjf.br>
2021-12-02 14:31:04 -0300

    Adicionado teste ServicoFactoryTest

---
+++ src/test/ServicoFactoryTest.java
@@ -0,0 +1,22 @@
+package test;
+
+import main.java.com.company.IServico;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ServicoFactoryTest {
+    public static IServico obterServico(String servico) {
+        Class classe = null;
+        Object objeto = null;
+        try {
+            classe = Class.forName("padroescriacao.factorymethod.Servico" + servico);
+            objeto = classe.newInstance();
+        } catch (Exception ex) {
+            throw new IllegalArgumentException("Serviço inexistente");
+        }
+        if (!(objeto instanceof IServico)) {
+            throw new IllegalArgumentException("Serviço inválido");
+        }
+        return (IServico) objeto;
+    }
+}
