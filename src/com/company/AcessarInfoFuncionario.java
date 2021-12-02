b4773be34034655231254d831080f4b6bf2759df
Rodrigo <rodrigo.elias@engenharia.ufjf.br>
2021-12-02 14:10:54 -0300

    Classe AcessarInfoFuncionario

---
+++ src/com/company/AcessarInfoFuncionario.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class AcessarInfoFuncionario {
+    public String executar() {
+        return "Acessando informações do funcionário.";
+    }
+
+    public String cancelar() {
+        return "Informações não acessadas.";
+    }
+}
